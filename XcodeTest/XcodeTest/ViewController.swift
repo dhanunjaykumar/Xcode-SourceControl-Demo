3ce0e92915694b0fe5b3fb410a28c55535fcd322
Dhanunjay <dhanunjay.mocharla@dineout.co.in>
2021-09-30 23:01:12 +0530

    project initial setup

---
+++ XcodeTest/XcodeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeTest
+//
+//  Created by Dhanunjay Kumar Mocharla on 30/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
