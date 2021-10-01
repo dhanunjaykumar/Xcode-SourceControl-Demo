aabe0ae1d43ab15cd39d3febcdc933b3d643fa52
Dhanunjay <dhanunjay.mocharla@dineout.co.in>
2021-10-01 12:39:41 +0530

    Demo Commit

---
+++ XcodeTest/XcodeTest/TestClass.swift
@@ -0,0 +1,10 @@
+//
+//  TestClass.swift
+//  XcodeTest
+//
+//  Created by Dhanunjay Kumar Mocharla on 01/10/21.
+//
+
+import Foundation
+
+//extension Uiv
--- XcodeTest/XcodeTest/ViewController.swift
+++ XcodeTest/XcodeTest/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
+enum Operator: String {
+    case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "/"
+}
+
+
 class ViewController: UIViewController {
+    
+    var str1 : String = "a"
+    var str2 : String = "b"
+    var str3 : String = "c"
+    
+    var tempStrng : String?
+    let currentOperator: Operator = .subtraction
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        if let tempStrng = tempStrng {
+//            <#body#>
+//        }
+     
+//        guard let tempStrng = tempStrng else {
+//            return
+//        }
+
+//        switch currentOperator {
+//        case .addition:
+//
+//        case .subtraction:
+//
+//        case .multiplication:
+//
+//        case .division:
+//            <#code#>
+//        }
+    }
+    @IBAction func btnClick(_ sender: Any) {
+        let str = coloumBreak()
+        print(str)
+    }
+    func coloumBreak()->String{
+        return str1 + str2 + str3
     }
 
 
 }
 
+extension ViewController{
+    
+}
